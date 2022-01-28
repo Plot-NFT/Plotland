76ae0c6f98640b1adc1e91956dfb9e5bf93e0d44
Dewa Bisma <idewagedebisma@gmail.com>
2022-01-28 22:16:27 +0800

    finish homepage herobanner interaction and animation

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -5,19 +5,46 @@ import Image from "react-bootstrap/Image";
 import styles from "./HeroBanner.module.scss";
 
 const HeroBanner = () => {
+  React.useEffect(() => {
+    const logo = document.querySelector("img#logo");
+    window.onkeydown = function (e) {
+      if (e.code === "Space") {
+        e.preventDefault();
+
+        logo.classList.add("pin");
+
+        setTimeout(() => {
+          logo.classList.remove("pin");
+        }, 1000);
+      }
+    };
+  }, []);
+
   return (
     <div className={styles.hero}>
       <div className={styles.logo}>
-        <Image src="/assets/Plot_Logo_Black.svg" alt="plot logo" />
+        <Image id="logo" src="/assets/Plot_Logo_Black.svg" alt="plot logo" />
       </div>
+
+      <Image
+        className={styles.triangle}
+        src="/assets/single-triangle.png"
+        alt="single triangle visual"
+      />
+
+      <Image
+        className={styles.doubleTriangle}
+        src="/assets/double-triangle.png"
+        alt="double triangle visual"
+      />
 
       <div className={styles.track}>
-        <Image src="/assets/track-1.png" alt="land visual" />
+        <Image
+          className={styles.land}
+          src="/assets/track.png"
+          alt="land visual"
+        />
       </div>
-
-      {/* <div className={styles.track2}>
-        <Image src="/assets/track-3.png" alt="land visual" />
-      </div> */}
     </div>
   );
 };
